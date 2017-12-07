bee85227d45cc9426a8629f242e1828f3266425b
gam5 <g.mcclellan@northeastern.edu>
2017-12-07 13:23:57 -0500

    Updated job number and improved error handling

--- Meyer_integration/schedule_meyer_job.pl
+++ Meyer_integration/schedule_meyer_job.pl
@@ -5,6 +5,8 @@
 
 use strict;
 use XML::Simple;
+use Data::Dumper;
+
 
 # Be sure to create a file containing your api_key called 'api_key.txt' in this directory
 my $api_key = `cat api_key.txt`;
@@ -13,10 +15,21 @@ if (! $api_key) { die "Please provide an API key in a file called 'api_key.txt' 
 
 my $api_server = 'api-na.hosted.exlibrisgroup.com';
 
-my $response_xml = `curl -H "Content-Type: application/xml" -X POST --data \@meyer_job.xml -s 'https://$api_server/almaws/v1/conf/jobs/S7599421650001401?apikey=$api_key&op=run'`;
+my $response_xml = `curl -H "Content-Type: application/xml" -X POST --data \@meyer_job.xml -s 'https://$api_server/almaws/v1/conf/jobs/S7696884050001401?apikey=$api_key&op=run'`;
+
 
 my $ref = XMLin($response_xml);
+#print Dumper($ref);
+
+if ($ref->{errorsExist} eq "true") {
+	open ERROR, ">>meyer_job_errors.txt" or die $!;
+	print ERROR `date`;
+	print ERROR $response_xml, "\n";
+	close ERROR;
+	exit;
+}
 
 open LOG, ">>meyer_job_log.txt" or die $!;
 print LOG $ref->{additional_info}->{content}, "\n";
 close LOG;
+
